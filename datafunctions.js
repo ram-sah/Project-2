b053b55895bae9c8f0012290ab7c0c18dc0411a5
Gary Burner <grburner@gmail.com>
2020-08-31 21:25:51 -0400

    basic data routing and query

--- datafunctions.js
+++ datafunctions.js
@@ -10,14 +10,17 @@ const getData = {
       where: {
         date: {
           [Op.lt]: moment("2020-08-26", ["YYYY-MM-DD"]), //change this to moment() once full data is live
-          [Op.gte]: moment("2020-08-26", ["YYYY-MM-DD"]).subtract(period, "days")
+          [Op.gte]: moment("2020-08-26", ["YYYY-MM-DD"]).subtract(
+            period,
+            "days"
+          )
         }
       }
     });
     return data;
   },
   getAllData: async () => {
-    const data = await db.sale.findAll({});
+    const data = await db.product.findAll({});
     return data;
   }
 };
--- routes/data-routes.js
+++ routes/data-routes.js
@@ -3,13 +3,13 @@
 const getData = require("../datafunctions.js");
 
 module.exports = function(app) {
-  // app.get("/:period", async (req, res) => {
-  //   console.log("into routing function");
-  //   const period = req.params.period;
-  //   const data = await getData.getSalesData(period);
-  //   res.json(data);
-  // });
-  app.get("/aaa", async (req, res) => {
+  app.get("/:period", async (req, res) => {
+    console.log("into routing function");
+    const period = req.params.period;
+    const data = await getData.getSalesData(period);
+    res.json(data);
+  });
+  app.get("/check", async (req, res) => {
     const data = await getData.getAllData();
     res.json(data);
   });
